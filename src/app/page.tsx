577f02745debdb41530629d1e76170e1078f6611
muhammadafzal <mafzalbalghari101@gmail.com>
2026-05-10 10:59:39 -0700

    Main homepage assembling all portfolio sections

---
+++ src/app/page.tsx
@@ -0,0 +1,27 @@
+import {
+  HeroSection,
+  ExperienceSection,
+  ProjectsSection,
+  DevOpsSection,
+  SkillsSection,
+  EducationSection,
+  CertificationsSection,
+  BlogSection,
+  ContactSection,
+} from "@/components/sections";
+
+export default function HomePage() {
+  return (
+    <>
+      <HeroSection />
+      <ExperienceSection />
+      <ProjectsSection />
+      <DevOpsSection />
+      <SkillsSection />
+      <EducationSection />
+      <CertificationsSection />
+      <BlogSection />
+      <ContactSection />
+    </>
+  );
+}
