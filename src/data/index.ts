909089afabe8ea9c65bbf056633b8ebb8143b9cf
muhammadafzal <mafzalbalghari101@gmail.com>
2026-05-11 10:21:43 -0700

    Barrel exports for data

---
+++ src/data/index.ts
@@ -0,0 +1,7 @@
+export { experience } from "./experience";
+export { projects } from "./projects";
+export { skillGroups } from "./skills";
+export { devopsTools } from "./devops";
+export { education } from "./education";
+export { certifications } from "./certifications";
+export { blogPosts } from "./blog";
